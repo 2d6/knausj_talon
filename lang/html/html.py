c096d186e268b61215793d6e496af34783e1a896
Moritz Wagner <moritz.wagner@diskordanz.de>
2023-11-16 18:28:09 +0100

    Add HTML language

---
+++ lang/html/html.py
@@ -0,0 +1,53 @@
+from talon import Context, Module, actions, settings
+from ...core.user_settings import get_list_from_csv
+
+ctx = Context()
+mod = Module()
+ctx.matches = r"""
+code.language: html
+code.language: javascriptreact
+code.language: typescriptreact
+"""
+
+default_tags = {
+    "html": "html",
+    "head": "head",
+    "body": "body",
+    "title": "title",
+    "meta": "meta",
+    "link": "link",
+    "script": "script",
+    "div": "div",
+    "span": "span",
+    "image": "img",
+    "bold": "b",
+    "italic": "i",
+}
+
+
+tags = get_list_from_csv(
+    "html_tags.csv",
+    headers=("Tag", "Spoken"),
+    default=default_tags,
+)
+
+mod.list("html_tag", desc="HTML tags")
+ctx.lists["self.html_tag"] = tags
+
+@mod.action_class
+class Actions:
+    def insert_html_tag(tag: str):
+        """Inserts a new tag of a given type"""
+
+    def insert_closed_html_tag(tag: str):
+        """Inserts a new closed tag of a given type"""
+
+
+
+@ctx.action_class("user")
+class UserActions:
+    def insert_html_tag(tag: str):
+         actions.user.insert_between(f"<{tag}>", f"</{tag}>")
+
+    def insert_closed_html_tag(tag: str):
+        actions.insert(f"<{tag} />")
--- lang/html/html.talon
+++ lang/html/html.talon
@@ -2,3 +2,6 @@ code.language: html
 code.language: javascriptreact
 code.language: typescriptreact
 -
+state tag {user.html_tag}: user.insert_html_tag("{html_tag}")
+state tag closed {user.html_tag}: user.insert_closed_html_tag("{html_tag}")
+state tag name {user.html_tag}: "{html_tag}"
